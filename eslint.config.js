200424ef57b8df041c34dacde70e1cf43d2ca41a
Lafllamme <teke900@gmail.com>
2025-06-21 22:11:46 +0200

    feat: update ESLint configuration to include TypeScript support and formatters

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,8 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-  unocss: true,
-  vue: true,
+    formatters: true,
+    unocss: true,
+    vue: true,
+    typescript: true,
 })
