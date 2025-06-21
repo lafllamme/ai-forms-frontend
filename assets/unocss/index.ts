97bee96f623211f6bdc0e061b10413d1caade8aa
Lafllamme <teke900@gmail.com>
2025-06-21 23:48:52 +0200

    style: format import statements for consistency

--- assets/unocss/index.ts
+++ assets/unocss/index.ts
@@ -1,6 +1,6 @@
-import {extendTheme} from './extendTheme'
-import {presets} from './presets'
-import {shortcuts} from './shortcuts'
-import {theme} from './theme'
+import { extendTheme } from './extendTheme'
+import { presets } from './presets'
+import { shortcuts } from './shortcuts'
+import { theme } from './theme'
 
-export {extendTheme, presets, shortcuts, theme}
+export { extendTheme, presets, shortcuts, theme }
