78ec792f860619887d411d5742c6ab5d9a17f3cc
Lafllamme <teke900@gmail.com>
2025-06-21 23:48:23 +0200

    feat: fetch data from API and display it in the component

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,21 @@
 <script lang="ts" setup>
 import ColorMode from '@/components/Buttons/ColorMode/ColorMode.vue'
+
+const { data } = await useFetch('/api/chat')
+
+consola.debug('Data fetched from API:', data.value)
 </script>
 
 <template>
   <div class="px-4 py-4">
     <div class="flex justify-center md:justify-start">
-      <h2 class="text-4xl antialiased tracking-tight font-sans font-light">
+      <h2 class="font-studio-feixen-serif-mono text-4xl font-medium tracking-tight antialiased">
         Welcome to AI Form Assistant
       </h2>
     </div>
-    <ColorMode/>
+    <div>
+      {{ data }}
+    </div>
+    <ColorMode />
   </div>
 </template>
