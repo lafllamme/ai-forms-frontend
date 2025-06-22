35c10630106f54c04af37a4550982b22545f933c
Lafllamme <teke900@gmail.com>
2025-06-22 21:58:39 +0200

    feat: refactor collapsible summary panel to position toggle button dynamically

--- pages/index.vue
+++ pages/index.vue
@@ -21,23 +21,41 @@ function toggleSummary() {
     <div class="w-full flex justify-center md:w-3/4">
       <FormChat />
     </div>
-    <!-- Collapsible summary panel -->
-    <!-- toggle button, open  section -->
-    <ToggleButton
-      :open="summaryOpen" @click="toggleSummary"
-    />
+    <!-- Collapsible summary panel WITH button inside -->
+    <div
+      :class="[
+        summaryOpen ? 'w-80 min-w-[20rem]' : 'w-0 min-w-0',
+      ]"
+      class="relative h-full transition-all duration-300 ease-in-out"
+      style="will-change: width;"
+    >
+      <!-- Button is on summary when open -->
+      <template v-if="summaryOpen">
+        <ToggleButton
+          :open="summaryOpen" class="absolute top-12 -left-7"
+          @click="toggleSummary"
+        />
+      </template>
 
-    <!-- Animated transition -->
-    <Transition mode="out-in" name="slide-panel">
-      <div
-        v-show="summaryOpen"
-        key="open"
-        class="fixed right-0 top-0 z-20 h-full w-80 flex flex-col justify-start bg-gray-6 p-2 py-24 color-sky-11 shadow-lg transition-all duration-300 <lg:hidden"
-      >
-        <CheatHeader />
-        <!-- More summary content -->
-      </div>
-    </Transition>
+      <Transition mode="out-in" name="slide-panel">
+        <div
+          v-show="summaryOpen"
+          key="open"
+          class="absolute inset-0 h-full w-80 flex flex-col justify-start border-l border-gray-2 rounded-r-2xl bg-gray-6 p-6 color-sky-11 shadow-lg transition-all duration-300"
+        >
+          <CheatHeader />
+          <!-- More summary content -->
+        </div>
+      </Transition>
+    </div>
+    <!-- Button is on screen edge when closed -->
+    <template v-if="!summaryOpen">
+      <ToggleButton
+        id="debug"
+        :open="summaryOpen" class="fixed right-0 top-12 z-50"
+        @click="toggleSummary"
+      />
+    </template>
   </div>
 </template>
 
